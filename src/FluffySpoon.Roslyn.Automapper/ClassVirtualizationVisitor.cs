471ffc2ec29eb56a7e14ebeb6fe086ccd049d51b
Mathias Lorenzen <mathias.lorenzen@live.com>
2019-12-15 11:17:07 +0100

    rename project.

---
+++ src/FluffySpoon.Roslyn.Automapper/ClassVirtualizationVisitor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace FluffySpoon.Roslyn.Automapper
+{
+    class ClassVirtualizationVisitor : CSharpSyntaxRewriter
+    {
+        private ICollection<string> _classes;
+
+        public ClassVirtualizationVisitor()
+        {
+            _classes = new HashSet<string>();
+        }
+
+        public override SyntaxNode VisitClassDeclaration(ClassDeclarationSyntax node)
+        {
+            node = (ClassDeclarationSyntax)base.VisitClassDeclaration(node);
+
+            string className = node.Identifier.ValueText;
+            _classes.Add(className); // save your visited classes
+
+            return node;
+        }
+    }
+}
